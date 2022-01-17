f9385ba82051cb4aa8790b2846959d99eeb4c52a
ms0ng <me@ms0ng.com>
2022-01-17 16:24:37 +0800

    update EventCenter

--- Assets/MSFrame/Events/EventCenter.cs
+++ Assets/MSFrame/Events/EventCenter.cs
@@ -16,23 +16,72 @@ namespace MSFrame.Events
         /// </summary>
         public int Register(string eventname, Action handler)
         {
-            return -1;
+            if (!mEvents.ContainsKey(eventname))
+            {
+                mEvents.Add(eventname, null);
+            }
+            int handerHash = handler.GetHashCode();
+
+            if (mEvents.TryGetValue(eventname, out Dictionary<int, Action> actions))
+            {
+                if (actions.ContainsKey(handerHash))
+                {
+                    //重复的方法
+                    return -1;
+                }
+                else
+                {
+                    actions.Add(handerHash, handler);
+                    return handerHash;
+                }
+            }
+            else
+            {
+                return -1;
+            }
         }
 
         /// <summary>
         /// 移除监听
         /// </summary>
-        public void RemoveRegister(string eventname)
+        public bool RemoveRegister(string eventname, Action handler)
         {
-
+            if (!mEvents.ContainsKey(eventname)) return false;
+            if (mEvents.TryGetValue(eventname, out Dictionary<int, Action> actions))
+            {
+                if (actions.ContainsKey(handler.GetHashCode()))
+                {
+                    actions.Remove(handler.GetHashCode());
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
         }
 
         /// <summary>
         /// 触发事件
         /// </summary>
-	    public void FireEvent(string eventname)
+	    public bool FireEvent(string eventname)
         {
-
+            if (mEvents.TryGetValue(eventname, out Dictionary<int, Action> actions))
+            {
+                foreach (var kvPair in actions)
+                {
+                    kvPair.Value?.Invoke();
+                }
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
     }
 }
