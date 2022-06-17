559b9b9db2c76aeba676a958ba656edfd9cb10a9
ms0ng <me@ms0ng.com>
2022-06-17 15:33:15 +0800

    Add YieldHelper

---
+++ Assets/MSFrame/RunningTime/YieldHelper.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MSFrame.RunningTime
+{
+    public class YieldHelper
+    {
+        public static Dictionary<float, WaitForSeconds> _waitForSecs;
+        public static WaitForSeconds WaitFor(float seconds)
+        {
+            if (_waitForSecs == null) _waitForSecs = new Dictionary<float, WaitForSeconds>();
+            if (_waitForSecs.TryGetValue(seconds, out var waitForSeconds))
+            {
+                return waitForSeconds;
+            }
+            else
+            {
+                WaitForSeconds wait = new WaitForSeconds(seconds);
+                _waitForSecs[seconds] = wait;
+                return wait;
+            }
+
+        }
+    }
+}
