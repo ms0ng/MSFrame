e745e018e2a20e4af3ee5bed4c36f940494a20ff
ms0ng <me@ms0ng.com>
2022-06-17 15:28:01 +0800

    Add ActionQueue

---
+++ Assets/MSFrame/RunningTime/ActionQueue.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MSFrame.RunningTime
+{
+    public class ActionQueue
+    {
+
+        private List<Action> _queueActions = new List<Action>();
+
+        public void Append(Action action)
+        {
+            _queueActions.Add(action);
+        }
+
+        public void Run()
+        {
+            foreach (var action in _queueActions)
+            {
+                action.Invoke();
+            }
+            _queueActions.Clear();
+        }
+    }
+}
